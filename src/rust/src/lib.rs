ee70f84ff9d097f737bfcd36fef640eb42026539
Hiroaki Yutani <yutani.ini@gmail.com>
2022-01-19 19:50:09 +0900

    Implement new DeviceDriver trait

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -1,12 +1,36 @@
-use extendr_api::prelude::*;
+use extendr_api::{
+    graphics::{device_descriptor::DeviceDescriptor, device_driver::DeviceDriver, DevDesc},
+    prelude::*,
+};
 
-mod graphic_device;
+struct YadnDevice<'a> {
+    welcome_message: &'a str,
+}
+
+impl<'a> DeviceDriver for YadnDevice<'a> {
+    fn activate(&mut self, _dd: DevDesc) {
+        let welcome_message = self.welcome_message;
+
+        rprintln!("🎉🍕🍰📺🍓✨🍣🐈🎿🎉🍕🍰📺🍓✨🍣🐈🎿");
+        rprintln!("");
+        rprintln!("   {welcome_message}   ");
+        rprintln!("");
+        rprintln!("🎉🍕🍰📺🍓✨🍣🐈🎿🎉🍕🍰📺🍓✨🍣🐈🎿");
+    }
+}
 
 /// Return string `"Hello world!"` to R.
+///
 /// @export
 #[extendr]
 fn yadngd(welcome_message: String) -> i32 {
-    graphic_device::make_graphic_device(welcome_message.as_str())
+    let device_driver = YadnDevice {
+        welcome_message: welcome_message.as_str(),
+    };
+
+    let device_descriptor = DeviceDescriptor::new();
+    let device = device_driver.create_device::<YadnDevice>(device_descriptor, "yadndgd");
+    device.device_number()
 }
 
 // Macro to generate exports.
